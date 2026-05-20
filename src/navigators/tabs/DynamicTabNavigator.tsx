2a58c1a0814dbe5c7718f78edcfb80874e8324fb
Aman <amanraj89969@gmail.com>
2026-05-20 23:03:01 +0530

    tab+stack navigation

--- src/navigators/tabs/DynamicTabNavigator.tsx
+++ src/navigators/tabs/DynamicTabNavigator.tsx
@@ -1,43 +1,42 @@
 import * as React from 'react';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import { Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
-import { Ionicons } from '@expo/vector-icons';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import HomeScreen from '../../screens/HomeScreen';
+import ProfileScreen from '../../screens/ProfileScreen';
+import DetailsScreen from '../../screens/DetailsScreen';
 
-function HomeScreen() {
-  return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Home Screen</Text>
-    </View>
-  );
-}
 
-function ProfileScreen() {
-  return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Profile Screen</Text>
-    </View>
-  );
-}
-
-function SettingsScreen() {
-  return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Settings Screen</Text>
-    </View>
-  );
-}
 
 const Tab = createBottomTabNavigator();
+const HomeStack = createNativeStackNavigator();
+
+function HomeStackNavigator() {
+  return (
+    <HomeStack.Navigator>
+      <HomeStack.Screen name="Home" component={HomeScreen} />
+      <HomeStack.Screen name="Details" component={DetailsScreen} />
+    </HomeStack.Navigator>
+  );
+}
+
+function MyTabs() {
+  return (
+    <Tab.Navigator>
+      <Tab.Screen
+        name="HomeTab"
+        component={HomeStackNavigator}
+        options={{ title: 'Home', headerShown: false }}
+      />
+      <Tab.Screen name="Profile" component={ProfileScreen} />
+    </Tab.Navigator>
+  );
+}
 
 export default function DynamicTabNavigator() {
   return (
     <NavigationContainer>
-      <Tab.Navigator initialRouteName='Profile' screenOptions={{ headerShown: false }}>
-        <Tab.Screen name="Home" component={HomeScreen} options={{ title: "Dashboard", tabBarLabel: "Home", tabBarIcon: ({ color }) => <Ionicons name="home" size={20} color={color} />, tabBarActiveTintColor: 'tomato' }} />
-        <Tab.Screen name="Profile" component={ProfileScreen} options={{ title: "Profile", tabBarLabel: "Profile", tabBarIcon: ({ color }) => <Ionicons name="person" size={20} color={color} />, tabBarActiveTintColor: 'tomato' }} />
-        <Tab.Screen name="Settings" component={SettingsScreen} options={{ title: "Settings", tabBarLabel: "Settings", tabBarIcon: ({ color }) => <Ionicons name="settings" size={20} color={color} />, tabBarActiveTintColor: 'tomato' }} />
-      </Tab.Navigator>
+      <MyTabs />
     </NavigationContainer>
   );
 }
